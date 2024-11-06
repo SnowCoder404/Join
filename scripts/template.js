ae16bcea4d070b269465b0f19efff1fb6a317727
SnowCoder404 <lenschaumarco@protonmail.com>
2024-11-06 20:23:44 +0100

    Add a header in the template.js

--- scripts/template.js
+++ scripts/template.js
@@ -7,3 +7,13 @@ function showHeader() {
                 <button class="bg-blue color-white sign-up ">Sign up</button>
             </div>`;
 }
+
+function showFooter() {
+    return `<div>
+                <img src="./assets/icon/icon.svg" class="image" alt="Join">
+            </div>
+            <div class="sign-in">
+                <span>Not a Join user ?</span>
+                <button class="bg-blue color-white sign-up ">Sign up</button>
+            </div>`;
+}
