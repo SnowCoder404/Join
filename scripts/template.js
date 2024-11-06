0a381be6dd42b2c8e094a0e93d636ff01763796f
SnowCoder404 <lenschaumarco@protonmail.com>
2024-11-06 19:36:25 +0100

    Add a header in the template.js

---
+++ scripts/template.js
@@ -0,0 +1,9 @@
+function showHeader() {
+    return `<div>
+                <img src="./assets/icon/icon.svg" class="image" alt="Join">
+            </div>
+            <div>
+                <span>Not a Join user ?</span>
+                <button>Sign up</button>
+            </div>`;
+}
