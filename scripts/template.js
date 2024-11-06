d5f5cd19279360deeda5908f028c6baed9a933c5
SnowCoder404 <lenschaumarco@protonmail.com>
2024-11-06 21:25:38 +0100

    Change the style

--- scripts/template.js
+++ scripts/template.js
@@ -2,7 +2,7 @@ function showHeader() {
     return `<div>
                 <img src="./assets/icon/icon.svg" class="image" alt="Join">
             </div>
-            <div class="sign-in">
+            <div class="d_flex_c sign-in">
                 <span>Not a Join user ?</span>
                 <button class="bg-blue color-white sign-up ">Sign up</button>
             </div>`;
