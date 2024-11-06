c892cf12da535a6b6d0e0194ee0fb34f841add17
mohamad-dev12 <programminglearn360@gmail.com>
2024-11-06 23:31:47 +0100

    Organize project

--- scripts/template.js
+++ scripts/template.js
@@ -1,19 +1,19 @@
 function showHeader() {
-    return `<div>
-                <img src="./assets/icon/icon.svg" class="image" alt="Join">
+  return `<div>
+                <img src="./assets/icon/icon.svg" class="join-icon" alt="Join">
             </div>
             <div id="joinUser" class="d_flex_c sign-in">
                 <span>Not a Join user ?</span>
-                <button class="bg-blue color-white sign-up ">Sign up</button>
+                <button class="sign-up">Sign up</button>
             </div>`;
 }
 
 function showFooter() {
-    return `<div>
-                <img src="./assets/icon/icon.svg" class="image" alt="Join">
+  return `<div>
+                <img src="./assets/icon/icon.svg" class="join-icon" alt="Join">
             </div>
             <div class="sign-in">
                 <span>Not a Join user ?</span>
-                <button class="bg-blue color-white sign-up ">Sign up</button>
+                <button class="sign-up">Sign up</button>
             </div>`;
 }
