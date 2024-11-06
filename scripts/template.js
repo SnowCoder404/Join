5a67f1d9e45a79e74a93ddb3ace48ea425622274
SnowCoder404 <lenschaumarco@protonmail.com>
2024-11-06 23:29:18 +0100

    Finish the style from login field and add the footer template.

--- scripts/template.js
+++ scripts/template.js
@@ -2,18 +2,15 @@ function showHeader() {
     return `<div>
                 <img src="./assets/icon/icon.svg" class="image" alt="Join">
             </div>
-            <div id="joinUser" class="d_flex_c sign-in">
+            <div class="d_flex_c sign-in hidden">
                 <span>Not a Join user ?</span>
                 <button class="bg-blue color-white sign-up ">Sign up</button>
             </div>`;
 }
 
 function showFooter() {
-    return `<div>
-                <img src="./assets/icon/icon.svg" class="image" alt="Join">
-            </div>
-            <div class="sign-in">
-                <span>Not a Join user ?</span>
-                <button class="bg-blue color-white sign-up ">Sign up</button>
+    return `<div class="d_flex_c sign-in">
+                <a><span>Privacy Policy</span><a>
+                <a><span>Legal notice</span><a>
             </div>`;
 }
