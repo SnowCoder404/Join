91dfc8fba2a1d7eb68c98512ebc179d31afcbac3
SnowCoder404 <lenschaumarco@protonmail.com>
2024-11-07 08:34:33 +0100

    The add task page is in work by snowcoder404 please do not change this file. Add the prio system with diferent color and add the add_task.js in the scripts folder

---
+++ scripts/add_task.js
@@ -0,0 +1,35 @@
+function loadTask() {
+    mediumPrio();
+}
+
+function checkThePrioOfTask(num) {
+    if (num == 1) {
+        urgentPrio();
+    } else if (num == 2) {
+        mediumPrio();
+    } else if (num == 3) {
+        lowPrio();
+    }
+}
+
+function urgentPrio() {
+    changeTheColor("urgent", "red");
+    changeTheColor("medium", "white");
+    changeTheColor("low", "white");
+}
+
+function mediumPrio() {
+    changeTheColor("urgent", "white");
+    changeTheColor("medium", "orange");
+    changeTheColor("low", "white");
+}
+
+function lowPrio() {
+    changeTheColor("urgent", "white");
+    changeTheColor("medium", "white");
+    changeTheColor("low", "green");
+}
+
+function changeTheColor(item, color) {
+    document.getElementById(item).style = `background-color: ${color}`;
+}
