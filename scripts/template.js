060e9c8297d207c06b8a3ae779b97fa221487839
SnowCoder404 <lenschaumarco@protonmail.com>
2024-11-07 18:35:00 +0100

    The add task page is in work by snowcoder404 please do not change this file. Add the prio system with diferent color and add the add_task.js in the scripts folder

--- scripts/template.js
+++ scripts/template.js
@@ -2,10 +2,9 @@ function sidebarShow() {
     return `<img class="logo" src="../assets/img/logo-white.png" alt="logo" />
             <div class="navigation">
                 <div class="active">
-                    <span class="active">
-                        <img src="../assets/icon/summary.png" alt="summary icon" />
-                        <a href="#">Summary</a>
-                    </span>
-                </div>
+                <a href="#" class="d_flex_c g_12 w-100">     
+                    <img src="../assets/icon/summary.png" alt="summary icon"/>    
+                    <span class="active">Summary</span>
+                </a>
             </div>`;
 }
