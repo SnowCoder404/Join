9e3193c601604428c0b15aacce825971f8c390c9
SnowCoder404 <lenschaumarco@protonmail.com>
2024-11-07 04:24:03 +0100

    Add the board.html and the sidebar menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,19 @@
-function init() {
-    document.getElementById("header").innerHTML = showHeader();
-    document.getElementById("footer").innerHTML = showFooter();
+function registerNewUser() {
+    let name = document.getElementById("name").value
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    let passwordConfirm = document.getElementById("passwordConfirm").value;
+    let checkBox = document.getElementById("checkBox").checked;
+    if (checkBox && password === passwordConfirm) {    
+        createNewUser(name, email, password)
+    }
 }
 
-function loginUser() {
-
-}
-
-function guestLogin() {
-    
+function createNewUser(name, email, password) {
+    let newUser = {
+        "name": name,
+        "email": email,
+        "password": password
+    }
+    console.log(newUser);
 }
