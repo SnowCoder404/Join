4ada9ba3ed8caf7f993ee50ec595d09585e36047
mohamad-dev12 <programminglearn360@gmail.com>
2024-11-07 18:47:47 +0100

    Fix: make entire hovered area as link

--- scripts/template.js
+++ scripts/template.js
@@ -2,30 +2,30 @@ function getSidebarHTML(pageName) {
   return `
     <img class="logo" src="../assets/img/logo-white.png" alt="logo" />
       <div class="navigation">
-        <div class="${pageName === 'summary' ? 'active' : ''}">
+        <a  href="../pages/summary.html" class="${pageName === 'summary' ? 'active' : ''}">
           <span>
             <img src="../assets/icon/summary.png" alt="summary icon" />
-            <a href="../pages/summary.html">Summary</a>
+            <p>Summary</p>
           </span>
-        </div>
-        <div class="${pageName === 'add_task' ? 'active' : ''}">
+        </a>
+        <a href="../pages/add_task.html" class="${pageName === 'add_task' ? 'active' : ''}">
           <span>
             <img src="../assets/icon/addTask.png" alt="add task icon" />
-            <a href="../pages/add_task.html">Add Task</a>
+            <p>Add Task</p>
           </span>
-        </div>
-        <div class="${pageName === 'board' ? 'active' : ''}">
+        </a>
+        <a href="../pages/board.html" class="${pageName === 'board' ? 'active' : ''}">
           <span>
             <img src="../assets/icon/board.png" alt="board icon" />
-            <a href="../pages/board.html">Board</a>
+            <p>Board</p>
           </span>
-        </div>
-        <div class="${pageName === 'contacts' ? 'active' : ''}">
+        </a>
+        <a href="../pages/contacts.html" class="${pageName === 'contacts' ? 'active' : ''}">
           <span>
             <img src="../assets/icon/contacts.png" alt="contacts icon" />
-            <a href="../pages/contacts.html">Contacts</a>
+            <p>Contacts</p>
           </span>
-        </div>
+        </a>
       </div>
       <div class="privacy-container">
         <a href="#">Privacy Policy</a>
