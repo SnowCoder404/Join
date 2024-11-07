138551951681fa22c7d8377d693229fcead47b35
SnowCoder404 <lenschaumarco@protonmail.com>
2024-11-07 18:57:58 +0100

    Finish the sidebar show function

--- scripts/template.js
+++ scripts/template.js
@@ -1,10 +1,29 @@
 function sidebarShow() {
-    return `<img class="logo" src="../assets/img/logo-white.png" alt="logo" />
+    return `<img class="logo" src="../assets/img/logo-white.png" alt="logo"/>
             <div class="navigation">
                 <div class="active">
-                <a href="#" class="d_flex_c g_12 w-100">     
-                    <img src="../assets/icon/summary.png" alt="summary icon"/>    
-                    <span class="active">Summary</span>
-                </a>
+                    <a href="#" class="d_flex_c g_12 w-100">     
+                        <img src="../assets/icon/summary.png" alt="summary icon"/>    
+                        <span class="active">Summary</span>
+                    </a>
+                 </div>   
+                <div>
+                    <a href="#" class="d_flex_c g_12 w-100">     
+                        <img src="../assets/icon/addTask.png" alt="summary icon"/>    
+                        <span>Add Task</span>
+                    </a>
+                </div> 
+                <div>
+                    <a href="#" class="d_flex_c g_12 w-100">     
+                        <img src="../assets/icon/board.png" alt="summary icon"/>    
+                        <span>Board</span>
+                    </a>
+                </div>   
+                <div>
+                    <a href="#" class="d_flex_c g_12 w-100">     
+                        <img src="../assets/icon/contacts.png" alt="summary icon"/>    
+                        <span>Contacts</span>
+                    </a>
+                </div> 
             </div>`;
 }
